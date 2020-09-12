efdc1d73d0ff3264e435d30d662ae25e8f2845af
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-09-12 20:50:25 +0200

    update wrappers to config

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import { mdsvex } from 'mdsvex'
+const extension = ['.md', '.svx']
 
-export const wrappers = {
-  svelte: (svelte, ctx) => {
-    svelte.preprocess = [
-      ...svelte.preprocess,
-      mdsvex({ extension: '.md', ...ctx.options })
-    ]
-    svelte.extensions = [].concat(
-      '.svelte',
-      '.md',
-      ctx.options.extensions,
-      svelte.extensions
-    )
-    return svelte
-  },
-  rollup: config => ({ ...config }),
-  routify: config => ({ ...config })
+export default {
+  hooks: [
+    {
+      condition: 'start',
+      action: (ctx, params) => ({ config: { mdsvex: {} } }), //provide an mdsvex object for the developer to modify
+    },
+    {
+      condition: 'afterUserConfig',
+      action: (ctx, params) => ({
+        config: {
+          svelte: {
+            preprocess: [
+              mdsvex({
+                extension: ['md', 'svx'],
+                // extension: params.extension || ctx.config.mdsvex.extension || extension,
+                ...ctx.config.mdsvex,
+                ...params
+              })
+            ],
+            extensions: ['.md', '.svx', '.svelte']
+            // extensions: params.extension || ctx.config.mdsvex.extension || extension
+          }
+        }
+      })
+    }
+  ]
 }
